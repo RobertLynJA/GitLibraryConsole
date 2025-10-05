7ab067b85cb5b59f44b6fe395131c30d86b2693a
Robert Lyn <lenny4422@gmail.com>
2025-10-05 10:23:54 -0400

    add ability to detect git folder

--- src/GitLibrary.Test/GitTests.cs
+++ src/GitLibrary.Test/GitTests.cs
@@ -1,10 +1,39 @@
-﻿namespace GitLibrary.Test;
+﻿using System;
+using System.IO.Abstractions;
+using NSubstitute;
+using Xunit;
+
+namespace GitLibrary.Test;
 
 public class GitTests
 {
     [Fact]
-    public void Test1()
+    public void IsGitRepository_ReturnsTrue_WhenGitDirectoryExists()
     {
-        
+        var dir = Substitute.For<IDirectoryInfo>();
+        var gitDir = Substitute.For<IDirectoryInfo>();
+        dir.Exists.Returns(true);
+        dir.GetDirectories(".git").Returns([gitDir]);
+
+        Assert.True(Git.IsGitRepository(dir));
+    }
+
+    [Fact]
+    public void IsGitRepository_ReturnsFalse_WhenDirectoryDoesNotExist()
+    {
+        var dir = Substitute.For<IDirectoryInfo>();
+        dir.Exists.Returns(false);
+
+        Assert.False(Git.IsGitRepository(dir));
+    }
+    
+    [Fact]
+    public void IsGitRepository_ReturnsFalse_WhenGitDirectoryDoesNotExist()
+    {
+        var dir = Substitute.For<IDirectoryInfo>();
+        dir.Exists.Returns(true);
+        dir.GetDirectories(".git").Returns([]);
+
+        Assert.False(Git.IsGitRepository(dir));
     }
 }
--- src/GitLibrary/Git.cs
+++ src/GitLibrary/Git.cs
@@ -1,6 +1,21 @@
 ﻿namespace GitLibrary;
 
-public class Git(DirectoryInfo _directoryInfo)     
+public class Git
 {
+    private readonly System.IO.Abstractions.IDirectoryInfo _directoryInfo;
     
+    public Git(System.IO.Abstractions.IDirectoryInfo directoryInfo)
+    {
+        _directoryInfo = directoryInfo ?? throw new ArgumentNullException(nameof(directoryInfo));
+
+        if (!IsGitRepository(_directoryInfo))
+        {
+            throw new ArgumentException("The provided path is not a Git repository.");
+        }
+    }
+    
+    public static bool IsGitRepository(System.IO.Abstractions.IDirectoryInfo directoryInfo)
+    {
+        return directoryInfo.Exists && directoryInfo.GetDirectories(".git").Length > 0;
+    }
 }
