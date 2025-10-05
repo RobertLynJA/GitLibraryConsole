71aa21c63fc739cd2f9ac3b3788e1e0a659ff08f
Robert Lyn <lenny4422@gmail.com>
2025-10-05 11:16:04 -0400

    add tests for constructor

--- src/GitLibrary.Test/GitTests.cs
+++ src/GitLibrary.Test/GitTests.cs
@@ -8,6 +8,34 @@ namespace GitLibrary.Test;
 public class GitTests
 {
     [Fact]
+    public void Constructor_ThrowsArgumentNullException_WhenDirectoryIsNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Git(null));
+    }
+
+    [Fact]
+    public void Constructor_ThrowsArgumentException_WhenNotAGitRepository()
+    {
+        var dir = Substitute.For<IDirectoryInfo>();
+        dir.Exists.Returns(true);
+        dir.GetDirectories(".git").Returns([]);
+
+        Assert.Throws<ArgumentException>(() => new Git(dir));
+    }
+
+    [Fact]
+    public void Constructor_Succeeds_WhenGitRepositoryExists()
+    {
+        var dir = Substitute.For<IDirectoryInfo>();
+        var gitDir = Substitute.For<IDirectoryInfo>();
+        dir.Exists.Returns(true);
+        dir.GetDirectories(".git").Returns([gitDir]);
+
+        var git = new Git(dir);
+        Assert.NotNull(git);
+    }
+    
+    [Fact]
     public void IsGitRepository_ReturnsTrue_WhenGitDirectoryExists()
     {
         var dir = Substitute.For<IDirectoryInfo>();
